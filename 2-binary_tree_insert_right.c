a072c328e3638db6384f092c92a121028278522a
danimohamed <mohameddani993@gmail.com>
2023-11-30 18:37:16 +0100

    feat: Add a function that measures the depth of a binary tree

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ * Return: pointer to the created node otherwise NULL
+ * on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	right_node = binary_tree_node(parent, value);
+	if (right_node == NULL)
+		return (NULL);
+
+	if (parent->right)
+	{
+		right_node->right = parent->right;
+		parent->right->parent = right_node;
+	}
+
+	parent->right = right_node;
+
+	return (right_node);
+}
